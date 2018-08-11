d46afc356e50914bf1076112cb9ba7735cff1f7c
David Vetrano <david.vetrano@gmail.com>
2018-08-11 00:10:43 -0700

    port python module to python 3

--- modules-and-plug-ins/python-module/example.py
+++ modules-and-plug-ins/python-module/example.py
@@ -1,27 +1,28 @@
 # need scikits audiolab for reading audio files
-from scikits.audiolab import wavread
+from scipy.io.wavfile import read
+import numpy as np
 
 # need to import btrack, our beat tracker
 import btrack
 
 # set the path to an audio file on your machine
-audioFilePath = "/path/to/your/audioFile.wav"
+audioFilePath = "rufus.wav"
 
 # read the audio file
-audioData, fs, enc = wavread(audioFilePath)     # extract audio from file
+rate, audioData = read(audioFilePath)     # extract audio from file
 
 # convert to mono if need be
 if (audioData[0].size == 2):
-    print "converting to mono"
-    data = np.average(data,axis=1)
+    print("converting to mono")
+    data = np.average(audioData, axis=1)
 
-# ==========================================    
-# Usage A: track beats from audio            
-beats = btrack.trackBeats(audioData)    
+# ==========================================
+# Usage A: track beats from audio
+beats = btrack.trackBeats(audioData)
 
 # ==========================================
 # Usage B: extract the onset detection function
-onsetDF = btrack.calculateOnsetDF(audioData)         
+onsetDF = btrack.calculateOnsetDF(audioData)
 
 # ==========================================
 # Usage C: track beats from the onset detection function (calculated in Usage B)
